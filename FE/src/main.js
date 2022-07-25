d627e97f2f172cdd59389541fffe5e96a2398780
LeerangKim <pwpw102@gmail.com>
2022-07-25 14:24:38 +0900

    init

--- FE/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import router from './router'
-import store from './store'
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-
-createApp(App).use(store).use(router).mount('#app')
-
