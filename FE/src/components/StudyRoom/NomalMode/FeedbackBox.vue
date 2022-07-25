ff5af74646a7ada5e6613f955d724f3725e442f5
lbh <hitlbh@naver.com>
2022-07-25 15:32:31 +0900

    docs: fe 오후 수정본

---
+++ FE/src/components/StudyRoom/NomalMode/FeedbackBox.vue
@@ -0,0 +1,27 @@
+// 이병헌
+<template>
+  <div class="FeedbackBox">
+    {{fb.content}}
+    <button @click="deleteFB">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "FeedbackBox",
+    props: {
+        fb:Object
+    },
+    setup(){
+        function deleteFB(){
+            store.dispacth('lbhModule/deleteFB', params) //store action에 추가
+
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
--- FE/src/views/StudyRoom/CLView.vue
+++ FE/src/views/StudyRoom/CLView.vue
@@ -7,12 +7,34 @@
 
   <!-- 하단 -->
   <!-- 탭 닫기 버튼? -->
+  <button @click="CloseTab"></button>
   </div>  
 </template>
 
 <script>
+import {onCreated} from 'vue'
+import { useStore } from 'vuex'
 export default {
+  name: 'CLView',
+  setup(){
+    onCreated(()=> {
+      // 면접자 정보 받아오기
+      // 면접자 이름, 면접자 자소서
+      const store = useStore();
+      const getEEInfo = store.dispatch("lbhModule/getEEInfo", params) //params에는 면접자 정보?
+      getEEInfo();
+    }),
+    function CloseTab () {
+      if(confirm("면접자의 자소서를 닫으시겠습니까?")){
+        close(); //현재 탭 닫기
+      }
+    }
 
+    return {
+      CloseTab,
+
+    }
+  },
 }
 </script>
 
