e20295ec5f0d383e7f9cadf6e51e47d3c9afdf07
LBH <hitlbh777@gmail.com>
2022-07-28 14:37:17 +0900

    style: 면접자실 버튼 수정

--- FE/src/views/StudyRoom/NormalMode/ERView.vue
+++ FE/src/views/StudyRoom/NormalMode/ERView.vue
@@ -47,7 +47,7 @@
       <div class="ERButtonFooter">
         <!-- 면접 완료 버튼(방장 유저) -->
         <div class="StudyDestroyBtn" @click="StudyDestroy" v-show="userType === 'superUser'">
-          <button><i class="bi bi-check-circle"></i></button>
+          <button><i class="bi bi-check-lg"></i></button>
         </div>
 
       </div>
