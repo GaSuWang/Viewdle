7285b2b72ba1f22b017193f26c92f0f3ae3c12c9
LeerangKim <pwpw102@gmail.com>
2022-07-31 14:53:50 +0900

    [#S07P12D111-77, S07P12D111-88] feat:소셜 로그인 구현

--- BE/src/main/java/com/ssafy/api/service/UserService.java
+++ BE/src/main/java/com/ssafy/api/service/UserService.java
@@ -4,10 +4,12 @@ import com.ssafy.api.request.UserRegisterPostReq;
 import com.ssafy.api.response.UserHistoryRes;
 import com.ssafy.db.entity.Common;
 import com.ssafy.db.entity.User;
+import org.springframework.stereotype.Service;
 
 /**
  *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
+@Service
 public interface UserService {
 	User createUser(UserRegisterPostReq userRegisterInfo);
 	User getUserByUserEmail(String userEmail);
--- BE/src/main/java/com/ssafy/common/oauth2/OAuth2SuccessHandler.java
+++ BE/src/main/java/com/ssafy/common/oauth2/OAuth2SuccessHandler.java
@@ -1,9 +1,16 @@
 package com.ssafy.common.oauth2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.api.request.UserRegisterPostReq;
+import com.ssafy.api.response.UserLoginPostRes;
+import com.ssafy.api.service.UserService;
+import com.ssafy.common.util.JwtTokenUtil;
+import com.ssafy.db.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -13,6 +20,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,30 +31,38 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final UserRequestMapper userRequestMapper;
     private final ObjectMapper objectMapper;
 
+    @Autowired
+    UserService userService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        UserDto userDto = userRequestMapper.toDto(oAuth2User);
+        String name = (String) oAuth2User.getAttributes().get("name");
+        String email = (String) oAuth2User.getAttributes().get("email");
+        String picture = (String) oAuth2User.getAttributes().get("picture");
 
         // 최초 로그인이라면 회원가입 처리를 한다.
+        User user =  userService.getUserByUserEmail(email);
+        if(user == null){
+            UserRegisterPostReq userInfo = new UserRegisterPostReq(email, name, "google", "google");
+            userService.createUser(userInfo);
 
-        Token token = tokenService.generateToken(userDto.getEmail(), "USER");
-        log.info("{}", token);
+        }
+
+        String token = JwtTokenUtil.getToken(email);
 
         writeTokenResponse(response, token);
     }
 
-    private void writeTokenResponse(HttpServletResponse response, Token token)
+    private void writeTokenResponse(HttpServletResponse response, String token)
             throws IOException {
-        response.setContentType("text/html;charset=UTF-8");
 
-        response.addHeader("Auth", token.getToken());
-        response.addHeader("Refresh", token.getRefreshToken());
-        response.setContentType("application/json;charset=UTF-8");
+        Map<String, String> resToken = new HashMap<>();
+        resToken.put("token", token);
 
         var writer = response.getWriter();
-        writer.println(objectMapper.writeValueAsString(token));
+        writer.println(objectMapper.writeValueAsString(resToken));
         writer.flush();
     }
 }
