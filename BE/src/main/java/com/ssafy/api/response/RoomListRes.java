0ecc33217266a0e9a94e902b9ea571aba29fe5e4
박채림 <from.htiek@gmail.com>
2022-08-02 11:21:49 +0900

    chore: 스터디 룸 목록 반환 API 수정

--- BE/src/main/java/com/ssafy/api/response/RoomListRes.java
+++ BE/src/main/java/com/ssafy/api/response/RoomListRes.java
@@ -16,11 +16,12 @@ public class RoomListRes {
     String roomRegTime;
     String roomActiveYN;
     String roomFullYN;
+    String thumbnailUrl;
 
 
-    // Q-type을 만들기위한 어노테이션 => 동작하는지 잘 모르겠음.
+    // Q-type을 만들기위한 어노테이션
     @QueryProjection
-    public RoomListRes(int roomSeq, int roomType, String roomTitle, String roomPrivateYN, int roomLimit, String roomRegTime, String roomActiveYN, String roomFullYN) {
+    public RoomListRes(int roomSeq, int roomType, String roomTitle, String roomPrivateYN, int roomLimit, String roomRegTime, String roomActiveYN, String roomFullYN, String thumbnailUrl) {
         this.roomSeq = roomSeq;
         this.roomType = roomType;
         this.roomTitle = roomTitle;
@@ -29,5 +30,6 @@ public class RoomListRes {
         this.roomRegTime = roomRegTime;
         this.roomActiveYN = roomActiveYN;
         this.roomFullYN = roomFullYN;
+        this.thumbnailUrl = thumbnailUrl;
     }
 }
