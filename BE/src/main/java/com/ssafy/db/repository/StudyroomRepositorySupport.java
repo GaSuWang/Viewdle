c8f5396a0a70f34f38d941432f48f68facd873e0
박채림 <from.htiek@gmail.com>
2022-08-03 13:58:10 +0900

    chore: 스터디 룸 목록 Response 수정

--- BE/src/main/java/com/ssafy/db/repository/StudyroomRepositorySupport.java
+++ BE/src/main/java/com/ssafy/db/repository/StudyroomRepositorySupport.java
@@ -1,15 +1,19 @@
 package com.ssafy.db.repository;
 
+import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.api.response.RoomListRes;
 import com.ssafy.db.entity.QCommon;
+import com.ssafy.db.entity.QParticipant;
 import com.ssafy.db.entity.QStudyroom;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.sql.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,6 +25,7 @@ public class StudyroomRepositorySupport {
     private final JPAQueryFactory jpaQueryFactory;
     QStudyroom studyroom = QStudyroom.studyroom;
     QCommon common = QCommon.common;
+    QParticipant participant = QParticipant.participant;
 
     public List<RoomListRes> findStudyroomByWhere(String order, Integer type, String privateYN, String FullYN){
         return jpaQueryFactory
@@ -29,6 +34,10 @@ public class StudyroomRepositorySupport {
                         studyroom.roomType,
                         studyroom.roomTitle,
                         studyroom.roomPrivateYN,
+                        JPAExpressions.select(participant.studyroom.count())
+                                .from(participant)
+                                .where(participant.studyroom.roomSeq.eq(studyroom.roomSeq),
+                                       participant.participantEnterYN.eq("Y")),
                         studyroom.roomLimit,
                         studyroom.roomRegTime,
                         studyroom.roomActiveYN,
@@ -75,6 +84,10 @@ public class StudyroomRepositorySupport {
                         studyroom.roomType,
                         studyroom.roomTitle,
                         studyroom.roomPrivateYN,
+                        JPAExpressions.select(participant.studyroom.count())
+                                .from(participant)
+                                .where(participant.studyroom.roomSeq.eq(studyroom.roomSeq),
+                                        participant.participantEnterYN.eq("Y")),
                         studyroom.roomLimit,
                         studyroom.roomRegTime,
                         studyroom.roomActiveYN,
