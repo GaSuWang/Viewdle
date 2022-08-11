b6999dc8c4be32c0e9263a8d1b7030ad91b167aa
박채림 <from.htiek@gmail.com>
2022-08-11 13:23:31 +0900

    chore: 임시 코드

--- FE/src/views/StudyRoom/EasyMode/EERView.vue
+++ FE/src/views/StudyRoom/EasyMode/EERView.vue
@@ -17,6 +17,8 @@
             :key="ER.stream.connection.connectionId"
             :stream-manager="ER"
           />
+          <video id="myvideo" hidden></video>
+          <canvas id="filterCanvas"></canvas>
         </div>
       </div>
     </div>
@@ -41,7 +43,7 @@
           </button>
         </div>
         <div class="StudyEnd">
-          <button>
+          <button>``
             <i class="bi bi-check-lg"></i>
           </button>
         </div>
@@ -65,11 +67,22 @@ import UserVideo from "@/components/UserVideo.vue";
 import { mapGetters } from "vuex";
 // import warningStackBar from "@/components/StudyRoom/EasyMode/WarningStackBar.vue"; //경고게이지 주석
 import { useRouter } from "vue-router";
+import * as facemesh from '@tensorflow-models/facemesh' 
+import '@tensorflow/tfjs'
+import '@tensorflow/tfjs-backend-wasm'
 
 export default {
   name: "EERView",
   components: { UserVideo }, // warningStackBar //경고게이지 주석
-  created() {
+  data() {
+    return {
+      warningCount: 0,
+      isFiltered: false,
+      // model : null, 
+      curFaces : [],
+    };
+  },
+  async created() {
     this.nextSuperUser = "";
     window.addEventListener("beforeunload", this.EERleaveSession);
     console.log("eerview cretaed", this.EE);
@@ -155,11 +168,53 @@ export default {
       }
     });
 
+    this.session.on("signal:videoFilterOn", (event) => {
+      console.log("=====" + event);
+      // 면접관 각각에게 넘겨주는 코드 구현할 것
+      if (JSON.parse(this.EE.stream.connection.data).clientData !== this.myUserName) {
+          this.renderPrediction();
+      }
+    })
+
     // 면접자일때만 음성인식 시작
     if (JSON.parse(this.EE.stream.connection.data).clientData === this.myUserName) {
       this.startRecognition();
     }
+
+    // 얼굴 인식 모델
+    // if (this.fmesh == null || this.fmesh == undefined) {
+    //   this.fmesh = new facemesh({
+    //     locateFile: (file) => {
+    //       return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    //     },
+    //   });
+    // }
+
+    // await tf.getBackend();
+    // this.model = await facemesh.load({detectionConfidence:0.9, maxFaces:3});
+    // console.log(this.model);
+    // this.filterCanvas = document.getElementById('filterCanvas');
+    // console.log('############################' + this.filterCanvas);
+    // let videoWidth = document.getElementById('filtervideo').width;
+    // let videoHeight = document.getElementById('filtervideo').height;
+    // this.filterCanvas.setAttribute("width", videoWidth);
+    // this.filterCanvas.setAttribute("height", videoHeight);
+    // this.ctx = this.filterCanvas.getContext('2d');
+
   },
+
+  async mounted() {
+      const video = document.getElementById("myvideo");
+      // Find the <video> element in the webpage, 
+      // then use the mediaDevices API to request a camera from the user
+      const stream = await navigator.mediaDevices.getUserMedia({
+        'video': {},
+      });
+      // Assign our camera to the HTML's video element
+      video.srcObject = stream;
+      video.play();
+  },
+
   setup() {
     const router = useRouter();
     //--------------------return waitingRoom ---------------------------
@@ -169,12 +224,6 @@ export default {
     //--------------------return waitingRoom end---------------------------
     return {
       returnWaitingRoom,
-    };
-  },
-  data() {
-    return {
-      warningCount: 0,
-      isFiltered: false,
     };
   },
   computed: {
@@ -189,6 +238,14 @@ export default {
       "SessionToken",
       "session",
       "currentUserList",
+      "OV",
+
+      //기기
+      "CameraSelected",
+      "CameraStatus",
+      "MicSelected",
+      "MicStatus",
+
     ]),
     nextSuperUserList() {
       return this.currentUserList.filter((p) => p.name !== this.myUserName);
@@ -318,7 +375,130 @@ export default {
           });
       }
     },
+
+    // send video filter signal
+    setVideoFilterOn() {
+      this.session
+        .signal({
+          data: "set video filter",
+          to: [],
+          type: "videoFilterOn",
+        })
+        .then(() => {
+          console.log("success");
+        })
+        .catch(() => {
+          console.log("failed");
+        });
+    },
+
+    changeOn(){
+      console.log('############################### changeOn');
+        const filterCanvas = document.getElementById('filterCanvas');
+        let im = filterCanvas.captureStream().getVideoTracks()[0];
+
+        this.session.unpublish(this.publisher);
+        
+        let publisher = this.OV.initPublisher(undefined, {
+          audioSource: this.MicSelected, 
+          videoSource: im, 
+          publishAudio: this.MicStatus, 
+          publishVideo: this.CameraStatus, 
+        });
+        
+        this.publisher.subscribeToRemote(true);
+        this.session.signal({
+          data: 'reconnect',
+          to: [],
+          type: 'publisherOn'
+          })
+        this.session.publish(publisher);
+    },
+
+    drawFace(face){
+      console.log('############################### drawFace');
+      const video = document.getElementById('myvideo')
+      const filterCanvas = document.getElementById('filterCanvas');
+      const ctx = filterCanvas.getContext('2d');
+
+      let mesh = face.scaledMesh;
     
+      // Left eye bounds (top, left, bottom, right) are the points (27, 130, 23, 243)
+      let lTop = mesh[27][1] - 3;
+      let lLeft = mesh[130][0] - 3;
+      let lBot = mesh[23][1] + 3;
+      let lRig = mesh[243][0] + 3;
+      let lWid = lRig-lLeft;
+      let lHei = lBot-lTop;
+      
+      // Right eye bounds (top, left, bottom, right) are the points (257, 463, 253, 359)
+      let rTop = mesh[257][1] - 3;
+      let rLeft = mesh[463][0] - 3;
+      let rBot = mesh[253][1] + 3;
+      let rRig = mesh[359][0] + 3;
+      let rWid = rRig-rLeft;
+      let rHei = rBot-rTop;
+
+      // Draw each eye from the video onto each eye in the canvas, but twice as big
+      ctx.drawImage(video, rLeft, rTop, rWid, rHei,
+                              (rLeft - rWid*.5) + 30, rTop - rHei*.5, 2*rWid, 2*rHei);
+      ctx.drawImage(video, lLeft, lTop, lWid, lHei,
+                              (lLeft - lWid*.5) - 30, lTop - lHei*.5, 2*lWid, 2*lHei);
+
+
+      // We're taking the X coordinates of the upper lip (which we assume are equal with lower lip)
+      let Xs = face.annotations.lipsUpperOuter.map(elem => elem[0])
+      // And the Y coordinates of the upper and lower lip 
+      let TopYs = face.annotations.lipsUpperOuter.map(elem => elem[1])
+      let BotYs = face.annotations.lipsLowerOuter.map(elem => elem[1])
+
+      let lipRight = Math.max(...Xs);
+      let lipLeft = Math.min(...Xs);
+      let lipTop = Math.min(...TopYs);
+      let lipBot = Math.max(...BotYs);
+      let lipWid = lipRight-lipLeft;
+      let lipHei = lipBot-lipTop;
+      ctx.drawImage(video, lipLeft, lipTop, lipWid, lipHei,
+                              lipLeft - lipWid*.5, lipTop - lipHei*.5, 2*lipWid, 2*lipHei);
+      this.changeOn();
+    },
+
+    /*drawVideo(){
+      const filterCanvas = document.getElementById('filterCanvas');
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, filterCanvas.width, canvas.height);
+      for (let face of this.curFaces){
+        this.drawFace(face);  
+      } 
+      // Call self again
+      requestAnimationFrame(this.drawVideo)
+    }, */
+
+    async renderPrediction() {
+      console.log('############################### renderPrediction');
+      console.log(this.model);
+      const video = document.getElementById('myvideo')
+      const filterCanvas = document.getElementById('filterCanvas');
+      const ctx = filterCanvas.getContext('2d');
+
+      // await tf.getBackend();
+      const model = await facemesh.load({detectionConfidence:0.9, maxFaces:3});
+      
+      const facepred = await model.estimateFaces(video);
+      if (facepred.length > 0) { 
+            this.curFaces = facepred;
+      }
+
+      console.log(this.curFaces);
+
+      ctx.clearRect(0, 0, filterCanvas.width, filterCanvas.height);
+
+      for (let face of this.curFaces){
+        this.drawFace(face);  
+      }
+
+    },
+
     // Speech API
     startRecognition() {
       let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
@@ -327,10 +507,11 @@ export default {
       recognition.lang = 'ko-KR';
       recognition.continuous = false;
     
-      recognition.start(); // 음성 인식 시작
+      recognition.start(); // start recognition
       console.log("start speech recognition");
-
-      recognition.onresult = function(e){ // 음성 인식 결과 반환
+      // let thisEE = JSON.parse(this.EE.stream.connection.data).clientData;
+      // let thisUserName = this.myUserName;
+      recognition.onresult = (e) => {
         for(let i = e.resultIndex; i < e.results.length; ++i){
           if(e.results[i].isFinal){ 
             let script = e.results[i][0].transcript;
@@ -338,13 +519,32 @@ export default {
             if(script.includes("빵")){
               alert("빵!");
             }else if(script.includes("감자")){
-              alert("감자!");
+              console.log("감자!");
+              this.setVideoFilterOn();
             }
           }
         }
       }
+      // recognition.onresult = function(e){ // 음성 인식 결과 반환
+      //   for(let i = e.resultIndex; i < e.results.length; ++i){
+      //     if(e.results[i].isFinal){ 
+      //       let script = e.results[i][0].transcript;
+      //       // console.log(script);
+      //       if(script.includes("빵")){
+      //         alert("빵!");
+      //       }else if(script.includes("감자")){
+      //         console.log("감자!");
+              
+      //         // if (JSON.parse(this.EE.stream.connection.data).clientData != this.myUserName) {
+      //         //   this.renderPrediction();
+      //         //   // this.renderPrediction().then(res => console.log(res));
+      //         // }
+      //       }
+      //     }
+      //   }
+      // }
 
-      recognition.onend = function(){ // 음성 인식이 끊겼을 때 
+      recognition.onend = function(){ // When voice recognition is stopped
         //recognition.stop();
         recognition.start(); 
       }     
@@ -352,8 +552,10 @@ export default {
       recognition.onerror = function(e) {
         console.log(e);
       }
-    }
+    },
   },
+  
+  
 };
 </script>
 
