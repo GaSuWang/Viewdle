8db58b18830997b222754087e97cc3023b2e1824
박채림 <from.htiek@gmail.com>
2022-08-14 23:06:48 +0900

    feat: 필터 목록 반환

--- BE/src/main/java/com/ssafy/db/repository/CommonRepository.java
+++ BE/src/main/java/com/ssafy/db/repository/CommonRepository.java
@@ -1,11 +1,15 @@
 package com.ssafy.db.repository;
 
+import com.ssafy.api.response.filterListRes;
 import com.ssafy.db.entity.Common;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommonRepository extends JpaRepository<Common, Long> {
 
     Common findByCommonSeq(int commonSeq);
+    List<filterListRes> findByImgGroup(String imgGroup);
 }
