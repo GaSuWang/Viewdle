c61327b70527eed309c78f6caad127d2ea1aefbf
박채림 <from.htiek@gmail.com>
2022-08-17 10:32:56 +0900

    design: 폰트 적용

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -12,6 +12,12 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+    font-family: 'S-CoreDream-3Light';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 
 .app {
   width: 100%;
@@ -20,6 +26,7 @@ export default {
   align-items: center;
   justify-content: center;
   background: #D8DBEB;
+  font-family:'S-CoreDream-3Light'
 }
 .app::-webkit-scrollbar {
   display: none;
