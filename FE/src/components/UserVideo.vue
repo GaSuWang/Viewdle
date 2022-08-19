b89d1bffdbcc1a54938c2159c4da4a6227cccaa5
이병헌 <hitlbh@naver.com>
2022-08-19 14:38:41 +0900

    UserVideo 주석 삭제 및 console.log 주석 처리

--- FE/src/components/UserVideo.vue
+++ FE/src/components/UserVideo.vue
@@ -1,6 +1,5 @@
 <template>
 <div class="userVideo" v-if="streamManager && showVid">
-	<!-- <div class="userInfo">{{ clientName }}</div> -->
 	<ov-video :stream-manager="streamManager"/>
 </div>
 </template>
@@ -32,21 +31,17 @@ export default {
 		},
 		showVid() {
 			const inWR = this.WRParticipantList.filter(p => p.myUserEmail === this.clientEmail)
-    // if(state.currentUserList.length) {
-    //   return state.currentUserList.filter(p => p.myUserEmail !== state.myUserEmail)
-    // } else return []
 			if(this.$router.currentRoute.value.name === 'waiting-room' && (inWR.length === 0)){
 				return false
 			} else return true
 		},
 	},
-	mounted(){
-		console.log()
-		console.log('uservideo mounted',this.session, this.streamManager)
+	// mounted(){
+	// 	console.log('uservideo mounted',this.session, this.streamManager)
 
-		console.log('지금 어디있지?', this.$router.currentRoute.value.name)
-		console.log('show this video?',this.videoStatus)
-	},
+	// 	console.log('지금 어디있지?', this.$router.currentRoute.value.name)
+	// 	console.log('show this video?',this.videoStatus)
+	// },
 	data(){
 		return{
 			videoStatus: true,
@@ -57,16 +52,6 @@ export default {
 			const { connection } = this.streamManager?.stream;
 			return JSON.parse(connection.data);
 		},
-		// async inWR() {
-		// 	await this.session.on('signal:publisherOn')
-		// 	if(this.streamManager){
-		// 		const vidUserEmail = JSON.parse(this.streamManager.stream.connection.data).clientEmail
-		// 		if(this.WRParticipantList.filter(e=> e.name === vidUserEmail).length === 0){
-		// 			this.videoStatus = false
-		// 		} else { 
-		// 			this.videoStatus = true}
-		// 	} else {this.videoStatus = false}
-		// },
 	},
 };
 </script>
@@ -80,7 +65,6 @@ export default {
 
 .userInfo{
     position: absolute;
-    /* justify-content: center; */
     background: #ffffff;
     width: 10%;
 	margin-left: 6%;
